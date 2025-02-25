2e63c394e2c3ce86cb0f6dc68cee86746abd3397
farzaadshojaei <farzaadshojaei@gmail.com>
2025-02-25 08:03:11 +0330

    BlockNumberPart1

--- src/system.rs
+++ src/system.rs
@@ -23,16 +23,36 @@ impl Pallet {
     //Return the current block number
     pub fn block_number(&self)->u32{
 
-        unimplemented!()
+        self.block_number
     }
 
     //Incrementing the block number, increases the block number by one
     pub fn inc_block_number(&mut self){
-        unimplemented!()
+       self.block_number= self.block_number.checked_add(1).unwrap();
 
     }
     //Increments the nonce of an account, for keep tracking of the amount of transactions
     pub fn inc_nance(&mut self,who: &String){
         unimplemented!()
     }
+
+}
+
+#[cfg!(test)]
+mod test{
+    use std::alloc::System;
+
+    #[test]
+    fn init_system(){
+        let system = super::Pallet::new();
+        assert_eq!(system.block_number, 0);
+    }
+    #[test]
+    fn inc_block_number(){
+        let mut system = super::Pallet::new();
+        system.inc_block_number();
+        assert_eq!(system.block_number, 1);
+    }
+
+
 }
